6ab6b12a11814b00bb0244de690bdfb9c9bb2152
Caden Cheng <cadencheng888@gmail.com>
2025-12-12 11:31:31 -0800

    Fixed bug in some_python_code.py

---
+++ hello.py
@@ -0,0 +1,3 @@
+print("hello, world")
+# python is an interpreter that can be run; the interpreter reads your program top to bottom
+# and translates it into the 0's and 1's that the computer can understand
